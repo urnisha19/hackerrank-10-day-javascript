c93d1deacad4165b90ea78139336c1bb083ec079
urnisha19 <urnisha09@gmail.com>
2021-06-10 02:26:43 +0600

    day 1 and 2

---
+++ Day 2/Loop.js
@@ -0,0 +1,35 @@
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    var vowels = [], constants = [];
+    for(var i=0; i<s.length; i++){
+        if(isVowel(s[i])){
+            vowels.push(s[i]);
+        }
+        else{
+            constants.push(s[i]);
+        }
+    }
+     printFormatted(vowels);
+    printFormatted(constants);
+}
+function printFormatted(arr){
+    for(var i=0; i<arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+function isVowel(x){
+    x = x.toLowerCase();
+    return x == 'a' || 
+           x == 'i' || 
+           x == 'e' || 
+           x == 'o' ||
+           x == 'u' ;
+}
+
+function main() {
+    const s = readLine();
+    vowelsAndConsonants(s);
+}
